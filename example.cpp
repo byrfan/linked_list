01ca7333b2bc0620f79f5b6b211a7b6899758f95
smlunar <61670949+moon0G@users.noreply.github.com>
2022-06-17 12:12:49 +1000

    Add files via upload

--- example.cpp
+++ example.cpp
@@ -1,13 +1,11 @@
 #include "llist.hpp"
 
 int main() {
-	Node* head = create_list(2);
-	append(head, 4);
-	append(head, 5);
-	head = push(head, 1);
-	insert_at_pos(head, 2, 3);
-	
-	//append(head, 10);
-	//head = push(head, 2);
-	print_list(head);
+	Node* head = create_list(2); // create new list, heads value is 2
+	append(head, 4); // append a new node with the value of 4
+	append(head, 5); // append a new node with the value of 5
+	head = push(head, 1); // push a new node to the start wwith the value of 1
+	insert_at_pos(head, 2, 3); // insert a new node at pos 2 with the value of 3
+
+	print_list(head); // prints the list
 }
